358f8d98d455edfc9eda134427d0768644a80fbe
Noire-Black-Heart <36617561+Noire-Black-Heart@users.noreply.github.com>
2018-09-22 16:26:41 +1000

    1

--- JAVA/poolGame/src/application/JsonSimpleReadExample.java
+++ JAVA/poolGame/src/application/JsonSimpleReadExample.java
@@ -1,18 +1,18 @@
 package application;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class JsonSimpleReadExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         JSONParser parser = new JSONParser();
 
@@ -49,8 +49,6 @@ public class JsonSimpleReadExample {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         }
 
