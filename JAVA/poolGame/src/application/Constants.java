190920aa5b6fa9d5d187f412b5b2b4e01bb8f891
Noire-Black-Heart <36617561+Noire-Black-Heart@users.noreply.github.com>
2018-10-02 00:29:38 +1000

    1

---
+++ JAVA/poolGame/src/application/Constants.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.*;
+
+import ballBuilder.Ball;
+import reader.TableHolder;
+
+public class Constants {
+	public static ArrayList<Ball> balls;
+	public static ArrayList<TableHolder> table;
+	public static int radius = 20;
+}
--- JAVA/poolGame/src/application/Main.java
+++ JAVA/poolGame/src/application/Main.java
@@ -2,25 +2,41 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import reader.*;
+import javafx.scene.shape.*;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import java.util.*;
-
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import ballBuilder.Ball;
 import facade.BallFacade;
 import facade.TableFacade;
 
 
 public class Main extends Application {
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			
 			
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root,600,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Paint.valueOf(Constants.table.get(0).getColour()));
+			
+			for(Ball b : Constants.balls) {
+				Circle ball = new Circle();
+				ball.setCenterX(b.getPositionX());
+		        ball.setCenterY(b.getPositionY());
+		        ball.setRadius(Constants.radius);
+				ball.setFill(Paint.valueOf(b.getColour())); 
+			   	root.getChildren().add(ball);
+				
+			}
+			
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -34,18 +50,18 @@ public class Main extends Application {
 		if (args.length > 0) {
 			configPath = args[0];
 		} else {
-			configPath = "config.json";
+			configPath = "config2.json";
 		}
 		
 		// parse the file:
 		BallFacade readBall = new BallFacade();
 		TableFacade readTable = new TableFacade();
 		
-		ArrayList<Ball> balls = readBall.FacadeParse(configPath);//ballFact.CreateReader().parse(configPath);
-		ArrayList<TableHolder> table = readTable.FacadeParse(configPath);
+		Constants.balls = readBall.FacadeParse(configPath);
+		Constants.table = readTable.FacadeParse(configPath);
 		
 		//load the configs into javafx
-		for(Ball b : balls) {
+		for(Ball b : Constants.balls) {
 			System.out.println(b.getPositionX());
 			
 		}
