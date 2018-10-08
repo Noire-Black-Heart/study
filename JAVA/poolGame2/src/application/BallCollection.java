eb82d6fb1fc587d63616c3919d2fafe49b95c972
Noire-Black-Heart <36617561+Noire-Black-Heart@users.noreply.github.com>
2018-10-09 00:38:37 +1100

    1

---
+++ JAVA/poolGame2/src/application/BallCollection.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.ArrayList;
+
+public class BallCollection {
+
+
+
+
+
+	 protected ArrayList<PoolBalls> nodes = new  ArrayList<PoolBalls>();
+
+	 public void addBall(PoolBalls ball) {
+		 nodes.add(ball);
+	 }
+	 public ArrayList<PoolBalls> getBalls(){
+		 return nodes;
+	 }
+}
