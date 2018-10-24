df696d5b9d949f1b8f5a8b50215bf9412bb540b1
Noire-Black-Heart <36617561+Noire-Black-Heart@users.noreply.github.com>
2018-10-25 09:17:15 +1100

    jj

--- JAVA/SampleBallGame/src/application/Main.java
+++ JAVA/SampleBallGame/src/application/Main.java
@@ -1,53 +1,40 @@
 package application;
+
+
+
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.LineBuilder;
 import javafx.stage.Stage;
-import javafx.scene.*;
-import javafx.scene.paint.*;
-import javafx.scene.shape.*;
-import javafx.scene.layout.*;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Colors");
+        Group root = new Group();
+        Scene scene = new Scene(root, 350, 300, Color.WHITE);
+        Line blackLine = LineBuilder.create()
+            .startX(170)
+            .startY(30)
+            .endX(20)
+            .endY(140)
+            .fill(Color.BLACK)
+            .strokeWidth(10.0f)
+            .translateY(20)
+            .build();
 
-@Override
-public void start(Stage primaryStage)
-{
-try {
-     
-      //Creating a Group object
-     Pane root = new Pane();  //The root of scene graph is a Group node
-     // Creating a Scene by passing the group object, height and width
-     Scene scene = new Scene(root,600,400); 
-     
- //RECTANGLE
-     //define a rectangle using its top-left corner, width and height
-     Rectangle rect = new Rectangle(0, 10, 30, 100);
-     //Use setFill to change the interior color.
-     rect.setFill(Color.BLUE);
-     //change the color and thickness of the border
-     rect.setStroke(Color.WHITESMOKE); 
-     rect.setStrokeWidth(10);
-     //This moves the x-coordinate of the top-left.
-     rect.setTranslateX(150);
-     //add the shape to the scene
-     root.getChildren().add(rect);
+        root.getChildren().add(blackLine); 
 
-      //setting color to the scene 
-       scene.setFill(Color.BLUE); 
-      //Setting the title to Stage. 
-       primaryStage.setTitle("JavaFX Basic Graphics Demo");
 
-     //Adding the scene to Stage 
-      primaryStage.setScene(scene);
-     //Displaying the contents of the stage 
-       primaryStage.show();
-    }  catch(Exception e)
-  {
-   e.printStackTrace();
-  }
- }
-
-public static void main(String[] args) 
-   {
-    launch(args);
-   }
-  }
+  
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
