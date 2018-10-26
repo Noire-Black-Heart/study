661fe52aae1c3eaf43e9e6753fe40666e5353e1e
Shuwei Zhang <szha3901@uni.sydney.edu.au>
2018-10-26 14:34:20 +1100

    111

--- JAVA/poolGame2/src/application/CuestickTest.java
+++ JAVA/poolGame2/src/application/CuestickTest.java
@@ -2,11 +2,16 @@ package application;
 
 public class CuestickTest {
 	public static void main(String[] args){
-		Cuestick stick1 = Cuestick.getInstance(10, 100, 100, 10);
+		Cuestick stick1 = Cuestick.getInstance();
 		
-		Cuestick stick2 = Cuestick.getInstance(20, 200, 200, 20);
+		System.out.println(stick1.stick.getStartX() + " " +stick1.stick.getEndY());
 		
-		System.out.println(stick1.getInstance(startX, startY, endX, endY));
-		System.out.println(stick2);
+		stick1.stick.setStartX(20);
+		stick1.stick.setEndY(50);
+		
+		Cuestick stick2 = Cuestick.getInstance();
+		
+		System.out.println(stick1.stick.getStartX() + " " +stick1.stick.getEndY());
+		System.out.println(stick2.stick.getStartX() + " " +stick2.stick.getEndY());
 	}
 }
